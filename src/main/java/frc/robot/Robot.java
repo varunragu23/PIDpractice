d83879da6e689df26f07101b936e439559ab7536
Varun Ragunath <ragunathd@varun-mac.local>
2021-10-23 13:31:15 -0700

    not building

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -15,10 +21,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  // Create constants for ports to be used in the code
+  private static final int kMotorPort = 7;
+  private static final int kJoystickPort = 0;
+  private static final int kUltrasonicPort = 0;
+
+  // Initialize objects 
+  private XboxController joystick = new XboxController(kJoystickPort); // Joystick
+  private final CANSparkMax neo = new CANSparkMax(kMotorPort, MotorType.kBrushless); // SparkMax motor controller
+  private final WPI_TalonFX falcon = new WPI_TalonFX(10); // TalonFX
+  private final AnalogInput ultrasonic = new AnalogInput(kUltrasonicPort); // Ultrasonic sensor
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -26,73 +39,58 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+
+    // Reset SparkMax & perform any needed clean up
+    neo.restoreFactoryDefaults();
+
+    // Same for TalonFX
+    falcon.configFactoryDefault();
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  @Override
+  public void teleopPeriodic() {
+
+    // Set speed of motors (value between [-1,1]) by
+    // taking input from joystick (value also between [-1, 1])
+    // and scaling it down (so things don't break)
+    // and using that value as the input
+    neo.set(joystick.getRawAxis(1) * 0.1);
+    // falcon.set(TalonFXControlMode.PercentOutput, joystick.getRawAxis(1) * 0.1);
+
+    // Get value from ultrasonic (12-bit value) and
+    // convert to inches
+    double distance = ultrasonic.getValue() * 0.0492;
+
+    // Display distance on Smart Dashboard & print
+    SmartDashboard.putNumber("Distance", distance);
+    // System.out.println("Distance: " + distance);
+  }
+
+
+  // Overriden methods we don't care about atm
+
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+  public void autonomousInit() {}
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
+  public void autonomousPeriodic() {}
 
-  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
